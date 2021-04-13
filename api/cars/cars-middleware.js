fd919b837ba540d90e0a6bf6dd5e89715b7b36ef
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-13 16:10:35 -0700

    show proper errors for checkCarPayload

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -19,10 +19,16 @@ const checkCarId = async (req, res, next) => {
 
 const checkCarPayload = (req, res, next) => {
   const { vin, make, model, mileage } = req.body;
-  if(vin && make && model && mileage){
-    next();
+  if(!vin){
+    res.status(400).json({ message: `vin is missing` });
+  } else if(!make){
+    res.status(400).json({ message: `make is missing` });
+  } else if(!model){
+    res.status(400).json({ message: `model is missing` });
+  } else if(!mileage){
+    res.status(400).json({ message: `mileage is missing` });
   } else {
-    res.status(400).json({ message: `vin, make, model, or mileage is missing` });
+    next();
   }
 }
 
